1ea9c7043eea603d16b8478daccf7d654ec4ea25
Gustavo H Rodrigues de Jesus <rodrigues.gustavoh@gmail.com>
2024-11-22 10:50:18 -0300

    :wrench:FSW06: chore: add tailwind prettier plugin

--- fsw6/finance-ai/app/page.tsx
+++ fsw6/finance-ai/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
-  return(
-    <h1>Hello, World!</h1>
+  return (
+    <div className="flex w-screen items-center justify-center">
+      <h1 className="p-5 text-red-500">Hello, World!</h1>
+    </div>
   );
 }
